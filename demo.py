2020a61dc037b0d44f41e277117cb534481f8f71
chLi <565353780@qq.com>
2022-07-14 14:45:46 +0800

    remove unused external

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#  from Module.trainer import demo
-from Module.detector import demo
+from Module.trainer import demo
+#  from Module.detector import demo
 
 if __name__ == "__main__":
     demo()
