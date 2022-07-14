c718ea497665adb2bbe5bddc69ae32882e898853
chLi <565353780@qq.com>
2022-07-14 14:33:43 +0800

    remove unused code

--- Method/dataloaders.py
+++ Method/dataloaders.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Method.total3d.dataloader import Total3D_dataloader
-from Method.mgnet.dataloader import MGNet_dataloader
 from Method.ldif.dataloader import LDIF_dataloader, HVD_LDIF_dataloader
 
--- Method/datasets.py
+++ Method/datasets.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Method.sun_rgbd.dataset import SUNRGBD
-from Method.total3d.dataset import Total3D_Dataset
 from Method.pix3d.dataset import PIX3D
-from Method.mgnet.dataset import MGNet_Dataset
 from Method.ldif.dataset import LDIF_Dataset
 
